def1c7f5c5739b054c7a7e640c32b6aa8f088e2a
Alonso del Arte <alonso.delarte@gmail.com>
2022-10-03 13:21:26 -0400

    Testing makeMenuItem( )

--- test/fractalviewer/ViewerNGTest.java
+++ test/fractalviewer/ViewerNGTest.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2021 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package fractalviewer;
 
@@ -9,6 +20,8 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
+
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 
@@ -21,22 +34,54 @@ import org.testng.annotations.Test;
  */
 public class ViewerNGTest {
     
+    // TODO: Delete menuItemsMatch() function after next commit
+    private static boolean menuItemsMatch(JMenuItem menuItemA, 
+            JMenuItem menuItemB) {
+        return menuItemA.getText().equals(menuItemB.getText()) 
+                && menuItemA.getAccessibleContext().getAccessibleDescription()
+                        .equals(menuItemB.getAccessibleContext()
+                                .getAccessibleDescription()) 
+                && menuItemA.getActionCommand()
+                        .equals(menuItemB.getActionCommand()) 
+                && menuItemA.getAccelerator().equals(menuItemB.getAccelerator()) 
+                && Arrays.equals(menuItemA.getActionListeners(), 
+                        menuItemB.getActionListeners());
+    }
+    
+    private static void assertMenuItemsMatch(JMenuItem actual, 
+            JMenuItem expected) {
+        assertEquals(actual.getText(), expected.getText(), 
+                "Menu texts should match");
+        assertEquals(actual.getAccessibleContext().getAccessibleDescription(), 
+                expected.getAccessibleContext().getAccessibleDescription(), 
+                "Accessible descriptions should match");
+        assertEquals(actual.getActionCommand(), expected.getActionCommand(), 
+                "Action commands should match");
+        assertEquals(actual.getAccelerator(), expected.getAccelerator(), 
+                "Accelerators should match");
+        assertEquals(actual.getActionListeners(), expected.getActionListeners(), 
+                "Arrays of action listeners should match");
+    }
+    
     /**
      * Test of makeMenuItem method, of class Viewer.
      */
     @Test
     public void testMakeMenuItem() {
         System.out.println("makeMenuItem");
-        String menuItemText = "";
-        String accessibleDescription = "";
-        String actionCommand = "";
-        KeyStroke accelerator = null;
+        String menuItemText = "Example command";
+        String accDescr = "Example command for testing purposes";
+        String actionCommand = "example";
+        KeyStroke accelerator = KeyStroke.getKeyStroke('X');
         Viewer instance = new ViewerImpl();
-        JMenuItem expResult = null;
-        JMenuItem result = instance.makeMenuItem(menuItemText, accessibleDescription, actionCommand, accelerator);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        JMenuItem expected = new JMenuItem(menuItemText);
+        expected.getAccessibleContext().setAccessibleDescription(accDescr);
+        expected.setActionCommand(actionCommand);
+        expected.setAccelerator(accelerator);
+        expected.addActionListener(instance);
+        JMenuItem actual = instance.makeMenuItem(menuItemText, accDescr, 
+                actionCommand, accelerator);
+        assertMenuItemsMatch(actual, expected);
     }
 
     /**
@@ -45,10 +90,10 @@ public class ViewerNGTest {
     @Test
     public void testLostOwnership() {
         System.out.println("lostOwnership");
-        Clipboard clipboard = null;
-        Transferable contents = null;
-        Viewer instance = new ViewerImpl();
-        instance.lostOwnership(clipboard, contents);
+//        Clipboard clipboard = null;
+//        Transferable contents = null;
+//        Viewer instance = new ViewerImpl();
+//        instance.lostOwnership(clipboard, contents);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -94,6 +139,7 @@ public class ViewerNGTest {
         public void mouseMoved(MouseEvent e) {
             throw new UnsupportedOperationException("Not supported yet.");
         }
+    
     }
     
 }
